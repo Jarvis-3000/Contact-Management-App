7e0311c2e4dd5ba451cb28f8066cd0504ea3fe62
Satish Gaud <sgaud2001@gmail.com>
2024-08-30 23:28:46 +0530

    added chart and map for covid-19 cases

--- src/pages/ChartsAndMaps/index.tsx
+++ src/pages/ChartsAndMaps/index.tsx
@@ -1,5 +1,17 @@
+import LineChart from "./LineChart";
+import Map from "./Map";
+
 function ChartsAndMaps() {
-  return <div>Charts And Maps</div>;
+  return (
+    <div className="w-full overflow-x-hidden overflow-y-auto">
+      <h1 className="text-center text-[30px] font-bold">Covid-19 Dashboard</h1>
+      <LineChart />
+      <br />
+      <br />
+      <p className="text-[20px] font-semibold text-center">Map</p>
+      <Map />
+    </div>
+  );
 }
 
 export default ChartsAndMaps;
--- src/pages/Contacts/NoContactFound.tsx
+++ src/pages/Contacts/NoContactFound.tsx
@@ -2,7 +2,7 @@ import NoContactIcon from "../../assets/noContact.svg";
 
 function NoContactFound() {
   return (
-    <div className="w-[100%] py-[100px] bg-white rounded-md m-auto p-4 border border-black text-center">
+    <div className="w-full py-[100px] bg-white rounded-md m-auto p-4 border border-black text-center">
       <div className="flex m-auto w-fit gap-4">
         <img src={NoContactIcon} alt="no contact icon" width={40}/>
         <text className="text-[30px] font-bold">No Contact Found</text>
